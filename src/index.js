a0cd2a8495f3dece32e3ec2d0e00130a561323fb
Pavel Voitihovich <pasha2003228@gmail.com>
2021-08-18 02:39:04 +0300

    feat: compleate the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments[0] === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let result = array.sort(function(a, b) {return a - b});
+  return result[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments[0] === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let result = array.sort(function(a, b) {return b - a});
+  return result[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments[0] === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let result = 0;
+  for (i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result / array.length;
 }
